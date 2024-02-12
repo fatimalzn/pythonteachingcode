952d45c2305865791e9ceb3a3f9f587a1b0570f7
fatimalzn <flozano2@students.kennesaw.edu>
2024-02-12 00:14:12 -0500

    Module 1

---
+++ Python Absolute Beginner/P1M1FatimaLozano.py
@@ -0,0 +1,15 @@
+# Create name check code
+
+# [ ] get input for input_test variable
+input_test = input("Who have you met in the last 24 hours?: ").lower()
+# [ ] print "True" message if "John" is in the input or False message if not
+print("You have met John = ", 'john' in input_test)
+
+# [ ] print True message if your name is in the input or False if not
+print("You have met Fatima = ", 'fatima' in input_test)
+
+# [ ] Challenge: Check if another person's name is in the input - print message
+print("You have met Johnathan = ", 'johnathan' in input_test)
+
+# [ ] Challenge: Check if a fourth person's name is in the input - print message
+print("You have met Suzanne = ", 'suzanne' in input_test)
