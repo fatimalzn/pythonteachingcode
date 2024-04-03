d56f900ec9ff872d12908d3b6c937be37d1671db
fatimalzn <flozano2@students.kennesaw.edu>
2024-04-03 01:05:32 -0400

    flask website

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -26,5 +26,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/fatima/')
+def fatima():
+    return render_template("lozano.html")
+
 if __name__=="__main__":
     app.run(debug=True)
